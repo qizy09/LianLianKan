f81f1a255f38fa5b36ffcb69010f43f980794822
Ziyang Qi <qizy09@gmail.com>
2015-03-04 09:03:49 -0800

    writing source codes

---
+++ lianliankan/def.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lianliankan
+{
+    class def
+    {
+        public const int MAXEDGE = 8;
+        public const int MAXPIC = 16;
+        public const int MAXTIME = 120;
+
+        public struct coordinate 
+        {
+            public int x;
+            public int y;
+        }
+
+        public struct queue
+        {
+            public coordinate place;
+            public int direct;
+            public int turn;
+        }
+    }
+}
