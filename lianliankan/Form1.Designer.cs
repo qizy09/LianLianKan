7559af0b52777dab68eecb823f5aea15b101c0ff
Ziyang Qi <qizy09@gmail.com>
2015-03-05 09:09:55 -0800

    writing source codes

---
+++ lianliankan/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace lianliankan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Label label1;
+            this.lblscore = new System.Windows.Forms.Label();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGameStart = new System.Windows.Forms.Button();
+            label3 = new System.Windows.Forms.Label();
+            label4 = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.BackColor = System.Drawing.Color.Black;
+            label3.Location = new System.Drawing.Point(560, 0);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(12, 600);
+            label3.TabIndex = 3;
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Font = new System.Drawing.Font("华文楷体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblscore.Location = new System.Drawing.Point(640, 121);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(34, 39);
+            this.lblscore.TabIndex = 2;
+            this.lblscore.Text = "0";
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("华文楷体", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbltime.Location = new System.Drawing.Point(640, 277);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(68, 39);
+            this.lbltime.TabIndex = 5;
+            this.lbltime.Text = "120";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(609, 515);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 38);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "              作者：Weeks\r\n清华大学软件学院九字班";
+            // 
+            // label4
+            // 
+            label4.Image = global::lianliankan.Properties.Resources.lbtime;
+            label4.Location = new System.Drawing.Point(600, 194);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(146, 53);
+            label4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.Image = global::lianliankan.Properties.Resources.lbscore;
+            label1.Location = new System.Drawing.Point(600, 45);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(146, 53);
+            label1.TabIndex = 1;
+            // 
+            // btnGameStart
+            // 
+            this.btnGameStart.BackgroundImage = global::lianliankan.Properties.Resources.btnst;
+            this.btnGameStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnGameStart.Location = new System.Drawing.Point(578, 426);
+            this.btnGameStart.Name = "btnGameStart";
+            this.btnGameStart.Size = new System.Drawing.Size(194, 60);
+            this.btnGameStart.TabIndex = 0;
+            this.btnGameStart.UseVisualStyleBackColor = true;
+            this.btnGameStart.Click += new System.EventHandler(this.btnGameStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::lianliankan.Properties.Resources.bgr;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(label4);
+            this.Controls.Add(label3);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.btnGameStart);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Form1";
+            this.Text = "连连看 - 女生节特别版";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGameStart;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lianliankan/llkfunction.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lianliankan
+{
+    class llkfunction
+    {
+        private int[,] map = new int[def.MAXEDGE, def.MAXEDGE];
+        private int[,] move = new int[4, 2];
+        private int[] picname = new int[def.MAXPIC + 1];
+        private bool gamest;
+        private int timest;
+        private int score;
+
+        public llkfunction()
+        {
+            move[0, 0] = 1; move[0, 1] = 0;
+            move[1, 0] = 0; move[1, 1] = 1;
+            move[2, 0] = -1; move[2, 1] = 0;
+            move[3, 0] = 0; move[3, 1] = -1;
+
+            int i;
+            for (i = 1; i <= def.MAXPIC; i++)
+            {
+                picname[i] = i;
+            }
+            gamest = false;
+            timest = 0;
+            score = 0;
+        }
+
+        public bool create()
+        {
+            int i, j, x, y, z;
+            int rest;
+            int[] num = new int[def.MAXPIC];
+            int[,] done = new int[def.MAXEDGE, def.MAXEDGE];
+            int[,] td = new int[def.MAXEDGE, def.MAXEDGE];
+            int[,] tmap = new int[def.MAXEDGE, def.MAXEDGE];
+
+            for (i = 0; i < def.MAXEDGE; i++)
+                for (j = 0; j < def.MAXEDGE; j++)
+                {
+                    done[i, j] = 0;
+                    tmap[i, j] = 0;
+                }
+            rest = def.MAXEDGE * def.MAXEDGE / 2;
+
+            Random pran = new Random();
+            
+            while (rest > 0)
+            {
+                for (i = 0; i < def.MAXEDGE; i++)
+                    for (j = 0; j < def.MAXEDGE; j++)
+                    {
+                        td[i, j] = done[i, j];
+                    }
+
+                for (i = 0; i < rest; i++)
+                {
+                    z = pran.Next(def.MAXPIC) + 1;
+                    x = pran.Next(def.MAXEDGE);
+                    y = pran.Next(def.MAXEDGE);
+                    while (td[x, y] != 0)
+                    {
+                        x = pran.Next(def.MAXEDGE);
+                        y = pran.Next(def.MAXEDGE);
+                    }
+                    tmap[x, y] = z;
+                    map[x, y] = z;
+                    td[x, y] = 1;
+                    while (td[x, y] != 0)
+                    {
+                        x = pran.Next(def.MAXEDGE);
+                        y = pran.Next(def.MAXEDGE);
+                    }
+                    tmap[x, y] = z;
+                    map[x, y] = z;
+                    td[x, y] = 1;
+                }
+
+                // to delete
+                int flag = 1, del, k, l, m;
+                def.queue[] que = new def.queue[def.MAXEDGE * def.MAXEDGE * 4];
+                def.coordinate c, ac;
+                int[,,] mark = new int[def.MAXEDGE, def.MAXEDGE, 4];
+                int f, r;
+
+                ac.x = 0; ac.y = 0;
+
+                while (flag == 1)
+                {
+                    flag = 0;
+                    for (i = 0; i < def.MAXEDGE; i++)
+                        for (j = 0; j < def.MAXEDGE; j++)
+                        {
+                            if (done[i, j] == 1) continue;
+                            del = 0;
+                            // try delete;
+                            
+                            for (l = 0; l < def.MAXEDGE; l++)
+                                for (m = 0; m < def.MAXEDGE; m++)
+                                    for (k = 0; k < 4; k++)
+                                    {
+                                        mark[l, m, k] = 1;
+                                    }
+
+                            f = 0; r = 0;
+                            for (l = 0; l < 4; l++)
+                            {
+                                c.x = i + move[l, 0];
+                                c.y = j + move[l, 1];
+                                if (c.x < 0 || c.y < 0 || c.x >= def.MAXEDGE || c.y >= def.MAXEDGE) continue;
+                                if (tmap[c.x, c.y] == tmap[i, j])
+                                {
+                                    del = 1;
+                                    ac = c;
+                                    break;
+                                }
+                                if (tmap[c.x, c.y] != 0) continue;
+                                mark[c.x, c.y, l] = 0;
+                                que[r].place = c;
+                                que[r].direct = l;
+                                que[r].turn = 0;
+                                r++;
+                            }
+                            
+                            while (del != 1 && f != r)
+                            {
+                                for (l = 0; l < 4; l++)
+                                {
+                                    c.x = que[f].place.x + move[l, 0];
+                                    c.y = que[f].place.y + move[l, 1];
+                                    if (c.x < 0 || c.y < 0 || c.x >= def.MAXEDGE || c.y >= def.MAXEDGE) continue;
+                                    if (tmap[c.x, c.y] == tmap[i, j])
+                                    {
+                                        del = 1;
+                                        ac = c;
+                                        break;
+                                    }
+                                    if (mark[c.x, c.y, l] == 0) continue;
+                                    if (tmap[c.x, c.y] != 0) continue;
+                                    if (que[f].direct != l && que[f].turn == 2) continue;
+                                    mark[c.x, c.y, l] = 0;
+                                    que[r].place = c;
+                                    que[r].direct = l;
+                                    que[r].turn = que[f].turn;
+                                    if (que[f].direct != l) que[r].turn++;
+                                    r++;
+                                }
+                                f++;
+                            }
+
+                            if (del == 1)
+                            {
+                                flag = 1;
+                                rest--;
+                                tmap[i, j] = 0;
+                                done[i, j] = 1;
+                                tmap[ac.x, ac.y] = 0;
+                                done[ac.x, ac.y] = 1;
+                            }
+                        }
+                }
+            }
+            gamest = true;
+            System.DateTime now = new System.DateTime();
+            now = System.DateTime.Now;
+            timest = now.Hour * 3600 + now.Minute * 60 + now.Second;
+            return true;
+        }
+
+        public bool gameisstart()
+        {
+            return gamest;
+        }
+
+        public int gettimestart()
+        {
+            return timest;
+        }
+
+        public int getscore()
+        {
+            return score;
+        }
+
+        public void addscore()
+        {
+            score += 10;
+        }
+
+        public int getMap(int x, int y)
+        {
+            return picname[map[x, y]];
+        }
+
+        public bool delete(def.coordinate a, def.coordinate b)
+        {
+            if (map[a.x, a.y] != map[b.x, b.y]) return false;
+            
+            def.queue[] que = new def.queue[def.MAXEDGE * def.MAXEDGE * 4];
+            def.coordinate c;
+            int[,,] mark = new int[def.MAXEDGE, def.MAXEDGE, 4];
+            int f, r, i, j, k;
+            int tmp;
+
+            tmp = map[b.x, b.y];
+            map[b.x, b.y] = 0;
+
+            for (i = 0; i < def.MAXEDGE; i++)
+                for (j = 0; j < def.MAXEDGE; j++)
+                    for (k = 0; k < 4; k++)
+                    {
+                        mark[i, j, k] = 1;
+                    }
+
+            f = 0; r = 0;
+            for (i = 0; i < 4; i++)
+            {
+                c.x = a.x + move[i, 0];
+                c.y = a.y + move[i, 1];
+                if (c.x < 0 || c.y < 0 || c.x >= def.MAXEDGE || c.y >= def.MAXEDGE) continue;
+                if (map[c.x, c.y] != 0) continue;
+                mark[c.x, c.y, i] = 0;
+                que[r].place = c;
+                que[r].direct = i;
+                que[r].turn = 0;
+                r++;
+            }
+
+            while (f != r)
+            {
+                for (i = 0; i < 4; i++)
+                {
+                    c.x = que[f].place.x + move[i, 0];
+                    c.y = que[f].place.y + move[i, 1];
+                    if (c.x < 0 || c.y < 0 || c.x >= def.MAXEDGE || c.y >= def.MAXEDGE) continue;
+                    if (mark[c.x, c.y, i] == 0) continue;
+                    if (map[c.x, c.y] != 0) continue;
+                    if (que[f].direct != i && que[f].turn == 2) continue;
+                    mark[c.x, c.y, i] = 0;
+                    que[r].place = c;
+                    que[r].direct = i;
+                    que[r].turn = que[f].turn;
+                    if (que[f].direct != i) que[r].turn++;
+                    r++;
+                }
+                f++;
+            }
+
+            for (k = 0; k < 4; k++)
+                if (mark[b.x, b.y, k] == 0)
+                {
+                    map[a.x, a.y] = 0;
+                    map[b.x, b.y] = 0;
+                    return true;
+                }
+            map[b.x, b.y] = tmp;
+            return false;
+        }
+    }
+}
